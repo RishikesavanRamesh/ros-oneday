5d2dfb6674d702b4b404a1afe1b0efa8f9a90fca
RishikesavanRamesh <84554651+RishikesavanRamesh@users.noreply.github.com>
2024-07-01 04:15:04 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Set the default build type
-BUILD_TYPE=RelWithDebInfo
-colcon build \
-        --symlink-install \
-        --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
-        -Wall -Wextra -Wpedantic
